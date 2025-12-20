12f55073d11c4aa3a20ccd262bd84a4b9c46a2f6
Himanshu <75300696+h-jangra@users.noreply.github.com>
2025-12-20 13:04:12 +0530

    Refactor ghost.sh to improve suggestion handling

--- ghost.sh
+++ ghost.sh
@@ -3,7 +3,6 @@
 _ghost_history_file="$HOME/.bash_history"
 _ghost_max_entries=10000
 _ghost_last_char=""
-_ghost_last_suggestion=""
 
 _ghost_load_history() {
     local count=0
@@ -26,33 +25,22 @@ _ghost_find_suggestion() {
 _ghost_accept() {
     local suggestion="$(_ghost_find_suggestion "$READLINE_LINE")"
     [[ -n "$suggestion" ]] && READLINE_LINE="${READLINE_LINE}${suggestion}" && READLINE_POINT="${#READLINE_LINE}"
-    _ghost_last_suggestion=""
 }
 
 _ghost_widget() {
     [[ -n "$_ghost_last_char" ]] && READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}${_ghost_last_char}${READLINE_LINE:$READLINE_POINT}" && ((READLINE_POINT++)) && _ghost_last_char=""
-    
     local suggestion="$(_ghost_find_suggestion "$READLINE_LINE")"
-    
-    # Only redraw if suggestion changed
-    if [[ "$suggestion" != "$_ghost_last_suggestion" ]]; then
-        # Clear old suggestion if it exists
-        [[ -n "$_ghost_last_suggestion" ]] && printf '\e[K'
-        # Show new suggestion
-        [[ -n "$suggestion" ]] && printf '\e[s\e[90m%s\e[0m\e[u' "$suggestion"
-        _ghost_last_suggestion="$suggestion"
-    fi
+    printf '\e7\r\e[K$ %s' "$READLINE_LINE"
+    [[ -n "$suggestion" ]] && printf '\e[90m%s\e[0m' "$suggestion"
+    printf '\e8'
 }
 
 _ghost_backspace() {
     (( READLINE_POINT > 0 )) && READLINE_LINE="${READLINE_LINE:0:$((READLINE_POINT-1))}${READLINE_LINE:$READLINE_POINT}" && ((READLINE_POINT--))
-    
     local suggestion="$(_ghost_find_suggestion "$READLINE_LINE")"
-    
-    # Clear to end of line and show new suggestion
-    printf '\e[K'
-    [[ -n "$suggestion" ]] && printf '\e[s\e[90m%s\e[0m\e[u' "$suggestion"
-    _ghost_last_suggestion="$suggestion"
+    printf '\e7\r\e[K$ %s' "$READLINE_LINE"
+    [[ -n "$suggestion" ]] && printf '\e[90m%s\e[0m' "$suggestion"
+    printf '\e8'
 }
 
 _ghost_load_history
@@ -69,4 +57,4 @@ for char in " " "-" "_" "/" "." "," "@" "=" "+"; do
     bind -x "\"$char\": _ghost_last_char='$char' _ghost_widget"
 done
 
-echo "Ghost.sh enabled - Right Arrow accepts suggestions"
+echo "Ghost enabled - Right Arrow accepts suggestions"
