96e8c7bb9b3da15bd87b6ef8f5ced189e1667fb8
h-jangra <manshu3220@gmail.com>
2026-03-03 20:14:57 +0530

    Index history by first char and improve stable rendering

--- ghost.sh
+++ ghost.sh
@@ -1,56 +1,104 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 _ghost_history_file="$HOME/.bash_history"
 _ghost_max_entries=10000
-_ghost_cache_line=""
-_ghost_cache_suggestion=""
+_ghost_last_render=""
+_ghost_suggestion=""
+
+declare -A _ghost_index
 
 _ghost_load_history() {
-    mapfile -t -n "$_ghost_max_entries" _ghost_history < <(tac "$_ghost_history_file" 2>/dev/null | grep -v '^$')
+    local line count=0
+    declare -A seen
+
+    mapfile -t lines < <(tail -n "$_ghost_max_entries" "$_ghost_history_file" 2>/dev/null)
+
+    for ((i=${#lines[@]}-1; i>=0; i--)); do
+        line="${lines[i]}"
+        line="${line%"${line##*[![:space:]]}"}"
+        [[ -z "$line" || -n "${seen[$line]+_}" ]] && continue
+        seen["$line"]=1
+        _ghost_index["${line:0:1}"]+="${line}"$'\n'
+        (( ++count >= _ghost_max_entries )) && break
+    done
 }
 
-_ghost_find_suggestion() {
+_ghost_find() {
     local prefix="$1"
+    
+    _ghost_suggestion=""
+
     [[ -z "$prefix" ]] && return
     
-    [[ "$prefix" == "$_ghost_cache_line" ]] && printf '%s' "$_ghost_cache_suggestion" && return
+    local bucket=${prefix:0:1}
+    local len=${#prefix}
     
-    local entry len_prefix="${#prefix}"
-    for entry in "${_ghost_history[@]}"; do
-        if [[ "$entry" == "$prefix"* ]] && (( ${#entry} > len_prefix )); then
-            _ghost_cache_line="$prefix"
-            _ghost_cache_suggestion="${entry:$len_prefix}"
-            printf '%s' "$_ghost_cache_suggestion"
-            return
-        fi
-    done
-    
-    _ghost_cache_line="$prefix"
-    _ghost_cache_suggestion=""
+    local entry
+    while IFS= read -r entry; do
+        [[ "$entry" == "$prefix"* && ${#entry} -gt len ]] && {
+            _ghost_suggestion="${entry:$len}"
+            break
+        }
+    done <<< "${_ghost_index[$bucket]}"
 }
 
 _ghost_render() {
-    local suggestion="$(_ghost_find_suggestion "$READLINE_LINE")"
-    printf '\e7\r\e[K$ %s' "$READLINE_LINE"
-    [[ -n "$suggestion" ]] && printf '\e[90m%s\e[0m' "$suggestion"
-    printf '\e8'
+    [[ $READLINE_POINT -ne ${#READLINE_LINE} ]] && return
+
+    local prefix="${READLINE_LINE:0:$READLINE_POINT}"
+    _ghost_find "$prefix"
+
+    local state="$READLINE_LINE|$_ghost_suggestion"
+
+    if [[ "${_ghost_last_render%%|*}" != "$READLINE_LINE" ]]; then
+        _ghost_last_render=""
+    fi
+
+    [[ "$state" == "$_ghost_last_render" ]] && return
+    _ghost_last_render="$state"
+
+    local p="${PS1@P}"
+    p="${p##*$'\n'}"
+
+    printf '\e[s\r\e[K%s%s' "$p" "$READLINE_LINE"
+
+    if [[ -n "$_ghost_suggestion" ]]; then
+        local first="${_ghost_suggestion:0:1}"
+        local rest="${_ghost_suggestion:1}"
+        printf '\e[1;38;5;250m%s\e[0m\e[38;5;245m%s\e[0m' "$first" "$rest"
+    fi
+
+    printf '\e[u'
 }
 
-_ghost_on_key() {
-    local char="$1"
-    READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}${char}${READLINE_LINE:$READLINE_POINT}"
+_ghost_insert() {
+    READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}${_ghost_key}${READLINE_LINE:$READLINE_POINT}"
     ((READLINE_POINT++))
+    _ghost_last_render=""
     _ghost_render
 }
 
 _ghost_accept() {
-    local suggestion="$(_ghost_find_suggestion "$READLINE_LINE")"
-    [[ -n "$suggestion" ]] && READLINE_LINE="${READLINE_LINE}${suggestion}" && READLINE_POINT="${#READLINE_LINE}" && _ghost_cache_line="" && printf '\e7\r\e[K$ %s\e[K\e8' "$READLINE_LINE"
+    [[ -z "$_ghost_suggestion" ]] && return
+
+    READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}${_ghost_suggestion}${READLINE_LINE:$READLINE_POINT}"
+    ((READLINE_POINT+=${#_ghost_suggestion}))
+
+    _ghost_last_render=""
+    _ghost_render
 }
 
 _ghost_backspace() {
-    (( READLINE_POINT > 0 )) && READLINE_LINE="${READLINE_LINE:0:$((READLINE_POINT-1))}${READLINE_LINE:$READLINE_POINT}" && ((READLINE_POINT--))
+    (( READLINE_POINT == 0 )) && return
+    READLINE_LINE="${READLINE_LINE:0:$((READLINE_POINT-1))}${READLINE_LINE:$READLINE_POINT}"
+    ((READLINE_POINT--))
+    _ghost_last_render=""
     _ghost_render
+}
+
+_ghost_bind_char() {
+    local key="$1"
+    bind -x "\"$key\": _ghost_key=$(printf '%q' "$key"); _ghost_insert"
 }
 
 _ghost_load_history
@@ -59,30 +107,11 @@ bind -x '"\e[C": _ghost_accept'
 bind -x '"\C-h": _ghost_backspace'
 bind -x '"\C-?": _ghost_backspace'
 
-# Alphanumerics
-for char in {a..z} {A..Z} {0..9}; do
-    eval "bind -x '\"$char\": _ghost_on_key $char'"
+for c in {a..z} {A..Z} {0..9} \
+         ' ' '-' '_' '/' '.' ',' '@' '=' '+' \
+         '>' '<' '|' '&' ':' ';' '*' '?' '!' '%' \
+         '#' '~' '^' '(' ')' '[' ']' '{' '}'; do
+    _ghost_bind_char "$c"
 done
-
-# Special characters - define handler functions to avoid quoting issues
-_ghost_key_space() { _ghost_on_key ' '; }
-_ghost_key_minus() { _ghost_on_key '-'; }
-_ghost_key_under() { _ghost_on_key '_'; }
-_ghost_key_slash() { _ghost_on_key '/'; }
-_ghost_key_dot() { _ghost_on_key '.'; }
-_ghost_key_comma() { _ghost_on_key ','; }
-_ghost_key_at() { _ghost_on_key '@'; }
-_ghost_key_equal() { _ghost_on_key '='; }
-_ghost_key_plus() { _ghost_on_key '+'; }
-
-bind -x '" ": _ghost_key_space'
-bind -x '"-": _ghost_key_minus'
-bind -x '"_": _ghost_key_under'
-bind -x '"/": _ghost_key_slash'
-bind -x '".": _ghost_key_dot'
-bind -x '",": _ghost_key_comma'
-bind -x '"@": _ghost_key_at'
-bind -x '"=": _ghost_key_equal'
-bind -x '"+": _ghost_key_plus'
 
 echo "Ghost enabled - Right Arrow accepts suggestions"
